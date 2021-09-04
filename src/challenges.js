3475cee510a7fb20307e11b89e2013379cbf1f90
Julia Barcelos <julaai@gmail.com>
2021-09-04 11:58:42 -0300

    Agora concluido o número 11

--- src/challenges.js
+++ src/challenges.js
@@ -23,18 +23,19 @@ function splitSentence(str) {
 // Desafio 4 'ÚLTIMO ITEM, PRIMEIRO ITEM' - Aqui eu busquei na documentação essas duas ferramentas de auxilio, o slice que serve pra retorna uma cópia de parte de um array, pode ser a posição exata ou o intervalo. Se der um intervalo ele pega o primeiro desse intervalo. Essa ferramenta o Fernando Gós que me alertou, enquanto eu tentava usar shift e pop que não dava certo. Usei também o concat para unir os arrays e join para juntar os dois em um array só e colocar um espaço entre eles.
 
 function concatName(arr) {
- let last = arr.slice(-1);
- let first = arr.slice(0, 1);
- let newarr = last.concat(first);
- return newarr.join(', ');
+  let last = arr.slice(-1);
+  let first = arr.slice(0, 1);
+  let newarr = last.concat(first);
+  return newarr.join(', ');
 }
 
 // Desafio 5 considere que cada vitória vale 3 pontos e cada empate vale 1 ponto.
-function footballPoints(wins, ties){
+function footballPoints(wins, ties) {
   return (wins * 3) + (ties)
 }
 
 // Desafio 6 demorei para desenvolver nessa porque ainda tenho dificuldade no chamar o conteúdo da posição do array.
+
 function highestCount(numbers) {
   let tallerId = 0;
   let cont = 0;
@@ -42,61 +43,51 @@ function highestCount(numbers) {
     if (numbers[key] > tallerId) {
       tallerId = numbers[key];
     }
-    if(numbers[key]< 0) {
+    if (numbers[key] < 0) {
       tallerId = numbers[key];
     }
   }
   for (let key in numbers) {
-    if (tallerId === numbers[key]){
-    cont += 1;
+    if (tallerId === numbers[key]) {
+      cont += 1;
     }
   }
   return cont
 }
 
-// Desafio 7 Muito massa, fiquei uma cara procurando a função módulo até a Laura Fumagalli me dizer, busca com outra palavra, tipo um numero absoluto. ai achei a ferramenta usada aqui na documentação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs  =)
+// Desafio 7 Muito massa, fiquei uma cara procurando a função módulo até a Laura Fumagalli me dizer, busca com outra palavra, tipo um numero absoluto. ai achei a ferramenta "Math.abs" na documentação =)
 
 function catAndMouse(mouse, cat1, cat2) {
-   if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)){
-   return "cat1"
- } else if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)){
-   return "cat2"
- } else {
-  return "os gatos trombam e o rato foge"
- }
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return "cat1"
+  } else if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
+    return "cat2"
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
 }
 // Desafio 8
-// precisei criar um array para que ele retornasse todas as strings que recebia. Dica recebida pelo grande Fernando Gós.
 
 function fizzBuzz(arr) {
   let newarr = [];
-  for (index = 0; index < arr.length; index+= 1) {
-    if (arr[index] % 3 === 0 && arr[index] % 5 ===0) {
-      newarr.push ("fizzBuzz");
-    }else if (arr[index] % 3 === 0) {
+  for (index = 0; index < arr.length; index += 1) {
+    if (arr[index] % 3 === 0 && arr[index] % 5 === 0) {
+      newarr.push("fizzBuzz");
+    } else if (arr[index] % 3 === 0) {
       newarr.push("fizz");
-    }else if (arr [index] % 5 ===0) {
+    } else if (arr[index] % 5 === 0) {
       newarr.push("buzz");
-    }else{
+    } else {
       newarr.push("bug!");
     }
   }
   return newarr
 }
-// Desafio 9  Pesquisei na documentação as opções que executavam essas trocas, mas não consegui implementar direto na string e preferi fazer essa transformação em array, como o Brunão me ajudou a pensar.
-// function encode(str) {
-//   for (let i = 0; i < str.length; i+= 1) {
-//     let new1 = str.replace('a', '1');
-//     new1 = str.replace('e', '2');
-//     new1= str.replace('i', '3');
-//     new1= str.replace('o', '4');
-//     new1 = str.replace('u', '5');
-//   }
-// return new1
-// }
+// Desafio 9  Pesquisei na documentação as opções que executavam essas trocas,achei o "replace" mas não consegui implementar direto, mesmo assistindo vídeos da Trybe, na string e preferi fazer essa transformação em array, como o Brunão me lembrou que poderia ser mais.
+
 function encode(str) {
   const arr = str.split('');
-  for(i = 0; i < arr.length; i+= 1){
+  for (i = 0; i < arr.length; i += 1) {
     if (arr[i] === 'a') {
       arr.splice(i, 1, '1');
     } else if (arr[i] === 'e') {
@@ -114,17 +105,17 @@ function encode(str) {
 
 function decode(str) {
   const arr = str.split('');
-  for(i = 0; i < arr.length; i+= 1){
+  for (i = 0; i < arr.length; i += 1) {
     if (arr[i] === '1') {
       arr.splice(i, 1, 'a');
     } else if (arr[i] === '2') {
-      arr.splice (i, 1, 'e');
+      arr.splice(i, 1, 'e');
     } else if (arr[i] === '3') {
-      arr.splice (i, 1, 'i');
+      arr.splice(i, 1, 'i');
     } else if (arr[i] === '4') {
-      arr.splice (i, 1, 'o');
+      arr.splice(i, 1, 'o');
     } else if (arr[i] === '5') {
-      arr.splice (i, 1, 'u');
+      arr.splice(i, 1, 'u');
     }
   }
   return arr.join('');
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,4 +1,5 @@
 // Desafio 10
+
 function techList(arrayTechs, name) {
   let arrayTechsSort = [];
   let arrayTechsObject = [];
@@ -7,84 +8,45 @@ function techList(arrayTechs, name) {
     return "Vazio!"
   } else {
     arrayTechsSort = arrayTechs.sort();
-    for (index = 0; index < arrayTechs.length; index += 1){
+    for (index = 0; index < arrayTechs.length; index += 1) {
       techs = {
         tech: arrayTechsSort[index],
         name: name
-        }
+      }
       arrayTechsObject.push(techs);
     }
     return arrayTechsObject;
   }
 }
-// Desafio 11
-// Retorne a string "Array com tamanho incorreto." caso o array tenha o tamanho diferente de 11
-// Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum dos números do array seja menor que 0
-// Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array seja maior que 9
-// Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array se repetir 3 vezes ou mais
-// Retorne um número de telefone, respeitando parênteses, traços e espaços caso os números do array estejam de acordo com as especificações
 
-// Se a função receber um array com tamanho diferente de 11, a mesma deve retornar "Array com tamanho incorreto.".
-// Caso algum dos números da array seja menor que 0, maior que 9 ou se repita 3 vezes ou mais, generatePhoneNumber deverá retornar a string "não é possível gerar um número de telefone com esses valores".
+// Desafio 11 - O mais difícil foi criar o contador, tentei de diferentes formas e não consegui fazer rodar, alguns colegas me mostram maneiras que eu não entendia, mas o Leandro Bonfim me apresentou essa forma de ver um array como um objeto (nesse caso) e funcionou na minha cabeça.
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-// function getCompare(array){
-//   for (index = 0; index < array.length; index += 1) {
-//     if (array[index] < 0 || array[index] > 9 ) {
-//       return "não é possível gerar um número de telefone com esses valores";
-//     } 
-//     let countNumber = 0;
-//     for(let i of array) {
-//       if(array[index] === i){
-//         countNumber += 1;
-//       }
-//     }
-//     if( countNumber > 2){
-//       return "não é possível gerar um número de telefone com esses valores";
-//     }
-//   }
-// }
-// console.log(getCompare(arr));
-
-
-// function generatePhoneNumber(foneNumbers) {
-//   if (foneNumbers.length !== 11) {
-//     return 'Array com tamanho incorreto.';
-//   }
-//   let getErro = false;
-//   for (let index = 0; index < foneNumbers.length; index += 1) {
-//     let countNumber = 0;
-//     if (foneNumbers[index] > 9 || foneNumbers[index] < 0) {
-//       getErro = true;
-//     }
-//     for (let number of foneNumbers) {
-//       if (foneNumbers[index] === number) {
-//         countNumber += 1;
-//       }
-//     }
-//     if (countNumber > 2) {
-//       getErro = true;
-//     }
-//   }
-//   if (getErro === true) {
-//     return 'não é possível gerar um número de telefone com esses valores';
-//   }
-//   return `(${foneNumbers[0]}${foneNumbers[1]}) ${foneNumbers[2]}${foneNumbers[3]}${foneNumbers[4]}${foneNumbers[5]}${foneNumbers[6]}-${foneNumbers[7]}${foneNumbers[8]}${foneNumbers[9]}${foneNumbers[10]}`;
-// }
-
-
-function generatePhoneNumber(array) {
-  
+function generatePhoneNumber(array){
+  let phoneNumber = [];
+  let numberCount = [0,0,0,0,0,0,0,0,0,0];
+  if (array.length !== 11) {
+    return `Array com tamanho incorreto.` 
+  }
+  for (index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9 ) {
+      return `não é possível gerar um número de telefone com esses valores`;
+    }
+    numberCount[array[index]] += 1;
+    if( numberCount[array[index]] >= 3){
+      return `não é possível gerar um número de telefone com esses valores`;
+    } 
+  }
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
 }
 
 // Desafio 12
 function triangleCheck() {
-  
+
 }
 
 // Desafio 13
 function hydrate() {
-  
+
 }
 
 module.exports = {
